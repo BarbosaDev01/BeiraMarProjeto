7badfd91e6db29abb279ad06712cb2bd1a79a233
BarbosaDev01 <barbosadev01@gmail.com>
2025-10-16 19:16:42 -0300

    Versão 2.1

---
+++ pages/login/confirmarSenha.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
+import { style } from './style';
+
+export default function darNome() {
+
+  return (
+    <ImageBackground>
+      source={require('../../assets/imagemFundo.png')}
+      style={style.container}
+      resizeMode="cover"
+     <View style={{ justifyContent: 'center', alignItems: 'center', marginBottom: 30 }}>
+                         <Image source={require('../../assets/logoBeira.png')} style={style.logo} />
+     </View>
+
+     <View>
+        <Text>Insira o código de recuperação</Text>
+     </View>
+     <View>
+        <input style={style.input} placeholder='-----'></input>
+     </View>
+
+     <View>
+        <button style={style.botoes}>
+            <Text style={style.botaoTexto}>Enviar código</Text>
+        </button>
+     </View>
+    </ImageBackground>
+  );
+}
